b6ec41585f7ef217f7dcb6eec5af246e07d21e59
zehrakarahan <zehrakarahan96@gmail.com>
2024-01-31 09:59:37 +0300

    Add project files.

---
+++ Capcha/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Capcha
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            pictureBox1 = new PictureBox();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(88, 210);
+            button1.Name = "button1";
+            button1.Size = new Size(286, 46);
+            button1.TabIndex = 0;
+            button1.Text = "submit";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(88, 52);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(286, 51);
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(88, 160);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(286, 27);
+            textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(88, 285);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(286, 27);
+            textBox2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(88, 18);
+            label1.Name = "label1";
+            label1.Size = new Size(92, 20);
+            label1.TabIndex = 4;
+            label1.Text = "Üretilen Kod";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(88, 123);
+            label2.Name = "label2";
+            label2.Size = new Size(146, 20);
+            label2.TabIndex = 5;
+            label2.Text = "Kodu Aşağıya Giriniz";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(pictureBox1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private PictureBox pictureBox1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label1;
+        private Label label2;
+    }
+}
---
+++ Capcha/Form1.cs
@@ -0,0 +1,63 @@
+﻿namespace Capcha
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == pictureBox1.Tag.ToString())
+            {
+                // Doğrulama başarılı
+                textBox2.Text = "basarili";
+            }
+            else
+            {
+                // Doğrulama başarısız
+                textBox2.Text = "basarisiz";
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string captchaCode = GenerateCaptchaCode();
+            pictureBox1.Image = GenerateCaptchaImage(captchaCode);
+            pictureBox1.Tag = captchaCode;
+        }
+        private Bitmap GenerateCaptchaImage(string captchaCode)
+        {
+            Bitmap bitmap = new Bitmap(200, 50);
+            using (Graphics g = Graphics.FromImage(bitmap))
+            {
+                g.Clear(Color.White);
+                using (Font font = new Font("Arial", 25, FontStyle.Bold))
+                {
+                    g.DrawString(captchaCode, font, Brushes.Gray, 2, 2);
+                }
+            }
+
+            return bitmap;
+        }
+        private string GenerateCaptchaCode()
+        {
+            string allowedChars = "ABCDEFGHJKLMNOPQRSTUVWXYZ0123456789";
+            Random rand = new Random();
+            char[] chars = new char[5];
+
+            for (int i = 0; i < chars.Length; i++)
+            {
+                chars[i] = allowedChars[rand.Next(0, allowedChars.Length)];
+            }
+
+            return new string(chars);
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
